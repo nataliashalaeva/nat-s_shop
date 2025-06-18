c1dc0bb0f4b2fdf3dd73571ff3dc3e7b415687df
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2025-06-18 22:18:58 +0300

    Create MainActivity.java

---
+++ mobile_client/java_files/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myshop;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.annotation.SuppressLint;
+import androidx.annotation.NonNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinButton, loginButton;
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinButton = (Button) findViewById(R.id.main_join_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent RegisterIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(RegisterIntent);
+                finish();
+            }
+        });
+
+    }
+}
